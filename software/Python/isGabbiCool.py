845cad0d6d47feb40f5c03e668ab13b6ded2900d
jaredgav <jaredgav@buffalo.edu>
2023-03-10 19:09:29 -0500

    Added a fun script talkin about how cool my gf is

---
+++ software/Python/isGabbiCool.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    while(True):
+        x = input()
+        if(x == "gabbi" or x == "gabrielle"):
+            print("She real cool, yo!")
+        elif(x == "jared"):
+            print("He got a cool gf")
+        elif(x == ""):
+            print("That ain't nobody")
+        else:
+            print("who even is that")
